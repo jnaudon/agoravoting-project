1b6d50db8da4bd49d18a0ab0f3a7eab203fd156d
Victor Ramirez <virako.9@gmail.com>
2015-12-30 20:34:22 +0100

    Added config file for jmeter and generator of hmac

---
+++ jmeter/generate_hmacs.py
@@ -0,0 +1,25 @@
+import hmac
+from time import time
+import hashlib
+
+shared_secret = b'SHARED' # Change this shared_secret by your shared_secret
+users = 50000
+usersIni = 1
+
+def get_hmac(userId, objType, objId, perm):
+    secret = shared_secret
+    now = 1000*long(time())
+    message = "%s:%s:%d:%s:%d" % (userId, objType, objId, perm, now)
+    _hmac = hmac.new(str(secret), str(message), hashlib.sha256).hexdigest()
+    khmac  = 'khmac:///sha-256;%s/%s' % (_hmac, message)
+    return ','.join([khmac, userId])
+
+hmacs = []
+total = 0
+ini = time()
+while total < users:
+    hmacs.append(get_hmac(str(total+usersIni), "AuthEvent", 2, "vote"))
+    total += 1
+with open('datas.csv', 'w') as f:
+    f.writelines('\n'.join(hmacs))
+print(time() - ini)
